8dc9b93a9e721651afcaeb16e1af48c9c47e0fc5
dhaura <dhaurapathirana@gmail.com>
2024-05-24 14:17:27 +0530

    Add correct opencv2 libs to target_link_libraries.

--- gsc-opencv.cpp
+++ gsc-opencv.cpp
@@ -4,20 +4,33 @@
 #include "./sequential/BasicIGSC.h"
 
 int main() {
-    // Load PNG image
-    std::string input_file;
-    std::cout << "Enter input file name: "; std::cin >> input_file; std::cout << "\n";
-    cv::Mat rgbaImage = cv::imread(input_file.c_str(), cv::IMREAD_UNCHANGED);
+    std::cout << "OpenCV version: " << CV_VERSION << std::endl;
+
+    // Load input image
+    std::string input_file = "D:/Academic/CPP/HPCProjects/project-img-greyscale-conversion/samples/inputs/sample-input-2.jpg";
+    std::cout << "Input file: " << input_file << "\n";
+
+    cv::Mat rgbaImage = cv::imread(input_file, cv::IMREAD_UNCHANGED);
+    if (rgbaImage.empty()) {
+        std::cerr << "Error: Could not load image. Check the file path.\n";
+        return -1;
+    }
     std::cout << "Loaded PNG image successfully.\n";
 
-    // Convert PNG image to grayscale
+    // Convert input image to grayscale
     cv::Mat greyImage;
     std::cout << "Starting grayscale conversion...\n";
     BasicIGSC::convertImageToGrayscale(rgbaImage, greyImage);
     std::cout << "Completed grayscale conversion successfully.\n";
 
     // Save grayscale image
-    cv::imwrite("D:/Academic/CPP/HPCProjects/project-img-greyscale-conversion/samples/outputs/sample-output-1.png", greyImage);
+    std::string output_file = "D:/Academic/CPP/HPCProjects/project-img-greyscale-conversion/samples/outputs/sample-output-2.jpg";
+    if (!cv::imwrite(output_file, greyImage)) {
+        std::cerr << "Error: Could not save the grayscale image.\n";
+        return -1;
+    }
+    std::cout << "Saved grayscale image to " << output_file << "\n";
+
 
     return 0;
 }
